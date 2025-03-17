d1bc9e65eeee73e8e61b1f1638c3b3532bed9fd4
nurainande <nurainande@gmail.com>
2025-03-17 10:55:53 +0100

    fixed image3

--- src/assets/index.js
+++ src/assets/index.js
@@ -1,6 +1,6 @@
 import IphoneImage from './Iphone-Image.png';
 import EarjamImage from './earjam.png';
-import MacbookImage from './Macbook.png';
+import MacbookImage from './MacBook.png';
 import PlaystationImage from './PlayStation.png';
 import VisionproImage from './visionPro.png';
 
